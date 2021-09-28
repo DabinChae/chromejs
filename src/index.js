ad5b74cb2f4264f46851f40014c2c89e1aba1070
DabinChae <vinda6578@gmail.com>
2021-09-28 18:09:40 +0900

    src2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ const myButton = document.querySelector("#my_number__form");
 const result = document.querySelector(".result");
 const myNumberSpan = document.querySelector("#my_number__span");
 const randomNumber = document.querySelector("#random_number");
-const resultSpan = document.querySelector("#result_span");
+const resultSpan = document.querySelector("#result__span");
 const HIDDEN = "hidden";
+
+function resultShow(myNumbervalue,myRandomvalue){
+    if(myNumbervalue==myRandomvalue){
+        resultSpan.innerText=`You won!`;
+    }else{
+        resultSpan.innerText=`You lose!`;
+    }
+}
+
 
 function onPlayClick(event) {
   event.preventDefault();
-  console.log("check");
-  // const value = myNumber.value;
-  // myNumberSpan.innerText = `${value}`;
-  // result.classList.remove("hidden");
+  const myNumbervalue = myNumber.value;
+  const myRandomvalue = Math.ceil(Math.random()*maxNumber.value);
+  myNumberSpan.innerText = `${myNumbervalue}`;
+  randomNumber.innerText = `${myRandomvalue}`;
+  resultShow(myNumbervalue,myRandomvalue);
+  result.classList.remove("hidden");
 }
+
+
 
 myButton.addEventListener("submit", onPlayClick);
